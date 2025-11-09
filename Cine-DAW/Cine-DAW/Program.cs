45e1a11d465610f190cb8c9274ca5c090fa4b978
antoine.lopez@educa.madrid.org <tu-email@github.com>
2025-11-09 13:48:50 +0100

    Implementada funcion menu principal y algunas constantes

--- Cine-DAW/Cine-DAW/Program.cs
+++ Cine-DAW/Cine-DAW/Program.cs
@@ -59,3 +59,93 @@ Configuracion PedirConfiguracion() {
         Columna = columna
     };
 }
+
+
+
+
+const int TamañoFila = 5;
+const int TamañoColumna = 8;
+
+
+const double PrecioEntrada = 6.50;
+
+struct Posicion {
+    int fila;
+    int columna;
+}
+
+enum Butaca {
+    Libre,
+    Ocupada,
+    FueraDeServicio
+}
+
+
+
+enum Menú {
+    VerSala,
+    ComprarEntrada,
+    DevolverEntrada,
+    Recaudación, 
+    Informe, 
+    Salir
+}
+
+void MostrarMenuPrincipal(Butaca[,] sala) {
+    int opcion = 0;
+    Menú opcionMenu = Menú.VerSala; // Inicialización para que esté disponible en el while
+
+    Console.WriteLine("Bienvenido al cine DAW");
+    Console.WriteLine("=====================");
+    Console.WriteLine("");
+
+    do {
+        Console.WriteLine("Opción 1: Visualizar sala");
+        Console.WriteLine("Opción 2: Comprar entrada");
+        Console.WriteLine("Opción 3: Devolver entrada");
+        Console.WriteLine("Opción 4: Recaudación total");
+        Console.WriteLine("Opción 5: Informe");
+        Console.WriteLine("Opción 6: Salir");
+
+        var inputOpcion = Console.ReadLine().Trim();
+
+        if (!int.TryParse(inputOpcion, out opcion) || opcion < 1 || opcion > 6) {
+            Console.WriteLine("❌ Entrada inválida. Intente nuevamente.");
+            continue;
+        }
+
+        opcionMenu = (Menú)(opcion - 1);
+
+        switch (opcionMenu) {
+            case Menú.VerSala:
+                imprimirSala(sala);
+                break;
+
+            case Menú.ComprarEntrada:
+                comprarEntrada(sala);
+                break;
+
+            case Menú.DevolverEntrada:
+                devolverEntrada(sala);
+                break;
+
+            case Menú.Recaudación:
+                calcularRecaudacion(sala);
+                break;
+
+            case Menú.Informe:
+                verInforme(sala);
+                break;
+
+            case Menú.Salir:
+                Console.WriteLine("Nos sentimos tristes que te vayas, vuelva otro díaaa");
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida");
+                break;
+        }
+
+    } while (opcionMenu != Menú.Salir);
+}
+
